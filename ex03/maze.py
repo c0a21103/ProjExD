ea8046acf0118f8ae19c98a99a5d4cb585cfe430
koki <c0a2110338@edu.teu.ac.jp>
2022-12-06 14:30:09 +0900

    maze

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,4 +5,8 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
+    cx, cy = 300, 400
+    tori = tk.PhotoImage(file="ex03/fig/4.png")
+    canvas.create_image(cx, cy, image=tori, tag="kokaton")
+    key = ""
     root.mainloop()
