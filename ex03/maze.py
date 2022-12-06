c6c85f61ddf27a9b487a0756eb9dfc6a2e4064c0
koki <c0a2110338@edu.teu.ac.jp>
2022-12-06 14:44:47 +0900

    maze-7

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,24 @@
 import tkinter as tk
 
+
+def key_down(event):
+    global key
+    key = event.keysym
+    
+
+def key_up(event):
+    global key
+    key = ""
+    
+def main_proc():
+    global cx, cy
+    if key == "Up": cy -= 20
+    if key == "Down": cy += 20
+    if key == "Left": cx -= 20
+    if key == "Right": cx += 20
+    canvas.coords("kokaton", cx, cy)
+    root.after(100,main_proc)
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -9,4 +28,7 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file="ex03/fig/4.png")
     canvas.create_image(cx, cy, image=tori, tag="kokaton")
     key = ""
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+    main_proc()
     root.mainloop()
