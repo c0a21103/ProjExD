6b9edeff797809283c3ac7e7d05328f5d1b0985e
koki <c0a2110338@edu.teu.ac.jp>
2022-12-13 17:00:19 +0900

    追加

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -11,11 +11,22 @@ def check_bound(obj_rct,scr_rct):
         tate = -1
     return yoko,tate
 
+def bombs(scrn_rct):
+    bomb_sfc = pg.Surface((20,20))
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255,0,0),(10,10),10)
+    pg.draw.circle(bomb_sfc, (255,0,0),(10,10),10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    return bomb_sfc,bomb_rct
 
 def main():        
     clock = pg.time.Clock()
     vx1, vy1 = +1, +1
     vx2, vy2 = +1, +1
+    vx3, vy3 = +1, +1
+    vx4, vy4 = +1, +1
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
     scrn_rct = scrn_sfc.get_rect()
@@ -27,16 +38,10 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
     
-    bomb_sfc = pg.Surface((20,20))
-    bomb_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bomb_sfc, (255,0,0),(10,10),10)
-    pg.draw.circle(bomb_sfc, (255,0,0),(10,10),10)
-    bomb_rct_1 = bomb_sfc.get_rect()
-    bomb_rct_2 = bomb_sfc.get_rect()
-    bomb_rct_1.centerx = random.randint(0, scrn_rct.width)
-    bomb_rct_1.centery = random.randint(0, scrn_rct.height)
-    bomb_rct_2.centerx = random.randint(0, scrn_rct.width)
-    bomb_rct_2.centery = random.randint(0, scrn_rct.height)
+    bomb_sfc,bomb_rct_1 = bombs(scrn_rct)
+    bomb_sfc,bomb_rct_2 = bombs(scrn_rct)
+    bomb_sfc,bomb_rct_3 = bombs(scrn_rct)
+    bomb_sfc,bomb_rct_4 = bombs(scrn_rct)
     
     fonto = pg.font.Font(None,100)
     fonto.set_bold
@@ -70,15 +75,25 @@ def main():
         
         bomb_rct_1.move_ip(vx1, vy1)
         bomb_rct_2.move_ip(vx2, vy2)
+        bomb_rct_3.move_ip(vx3, vy3)
+        bomb_rct_4.move_ip(vx4, vy4)
         scrn_sfc.blit(bomb_sfc,bomb_rct_1)
         scrn_sfc.blit(bomb_sfc,bomb_rct_2)
+        scrn_sfc.blit(bomb_sfc,bomb_rct_3)
+        scrn_sfc.blit(bomb_sfc,bomb_rct_4)
         yoko1, tate1 = check_bound(bomb_rct_1,scrn_rct)
         yoko2, tate2 = check_bound(bomb_rct_2,scrn_rct)
+        yoko3, tate3 = check_bound(bomb_rct_3,scrn_rct)
+        yoko4, tate4 = check_bound(bomb_rct_4,scrn_rct)
         vx1 *= yoko1
         vy1 *= tate1
         vx2 *= yoko2
         vy2 *= tate2
-        if tori_rct.colliderect(bomb_rct_1) or tori_rct.colliderect(bomb_rct_2):
+        vx3 *= yoko3
+        vy3 *= tate3
+        vx4 *= yoko4
+        vy4 *= tate4
+        if tori_rct.colliderect(bomb_rct_1) or tori_rct.colliderect(bomb_rct_2) or tori_rct.colliderect(bomb_rct_3) or tori_rct.colliderect(bomb_rct_4):
             scrn_sfc.blit(txt,scrn_rct.center)
             pg.display.update()
             pg.time.wait(3000)
