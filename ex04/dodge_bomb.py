065af2e6636c6f231f6d66b68962c41ba5a9d353
koki <c0a2110338@edu.teu.ac.jp>
2022-12-13 14:32:09 +0900

    練習４

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -12,13 +12,24 @@ def main():
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
-    scrn_sfc.blit(tori_sfc,tori_rct)
+    
 
     while True:
         scrn_sfc.blit(bg_sfc,bg_rct)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+        key_dct = pg.key.get_pressed()
+        if key_dct[pg.K_UP]:
+            tori_rct.centery -= 1
+        if key_dct[pg.K_DOWN]:
+            tori_rct.centery += 1
+        if key_dct[pg.K_LEFT]:
+            tori_rct.centerx -= 1
+        if key_dct[pg.K_RIGHT]:
+            tori_rct.centerx += 1
+            
+        scrn_sfc.blit(tori_sfc,tori_rct)
         pg.display.update()
         clock.tick(1000)
             
