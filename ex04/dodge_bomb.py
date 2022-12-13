45753d18b9a5ffd62cee27601d199743de3710c7
koki <c0a2110338@edu.teu.ac.jp>
2022-12-13 18:03:19 +0900

    修正版#9

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -23,35 +23,43 @@ def bombs(scrn_rct):
 
 def main():        
     clock = pg.time.Clock()
+    # 初期値の設定
     vx1, vy1 = +1, +1
     vx2, vy2 = +1, +1
     vx3, vy3 = +1, +1
     vx4, vy4 = +1, +1
+    
+    #背景の描画準備
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
     scrn_rct = scrn_sfc.get_rect()
     bg_sfc = pg.image.load("ex04/fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
     
+    #こうかとんの描画準備
     tori_sfc = pg.image.load("ex04/fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
     
+    #爆弾の描画準備
     bomb_sfc,bomb_rct_1 = bombs(scrn_rct)
     bomb_sfc,bomb_rct_2 = bombs(scrn_rct)
     bomb_sfc,bomb_rct_3 = bombs(scrn_rct)
     bomb_sfc,bomb_rct_4 = bombs(scrn_rct)
     
+    #ゲームオーバーの文字の描画準備
     fonto = pg.font.Font(None,100)
     fonto.set_bold
     txt = fonto.render("GAME OVER", True, (255,255,255))
+    
     while True:
         scrn_sfc.blit(bg_sfc,bg_rct)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
         
+        # こうかとんを動かす処理
         key_dct = pg.key.get_pressed()
         if key_dct[pg.K_UP]:
             tori_rct.centery -= 1
@@ -61,7 +69,8 @@ def main():
             tori_rct.centerx -= 1
         if key_dct[pg.K_RIGHT]:
             tori_rct.centerx += 1
-        
+            
+        #こうかとんが壁にぶつかった時の処理
         if check_bound(tori_rct,scrn_rct) != (+1, +1):
             if key_dct[pg.K_UP]:
                 tori_rct.centery += 1
@@ -71,8 +80,10 @@ def main():
                 tori_rct.centerx += 1
             if key_dct[pg.K_RIGHT]:
                 tori_rct.centerx -= 1
+        
         scrn_sfc.blit(tori_sfc,tori_rct)
         
+        # 爆弾の移動処理
         bomb_rct_1.move_ip(vx1, vy1)
         bomb_rct_2.move_ip(vx2, vy2)
         bomb_rct_3.move_ip(vx3, vy3)
@@ -93,12 +104,13 @@ def main():
         vy3 *= tate3
         vx4 *= yoko4
         vy4 *= tate4
+        
+        # ゲームオーバーの判定
         if tori_rct.colliderect(bomb_rct_1) or tori_rct.colliderect(bomb_rct_2) or tori_rct.colliderect(bomb_rct_3) or tori_rct.colliderect(bomb_rct_4):
             scrn_sfc.blit(txt,scrn_rct.center)
             pg.display.update()
             pg.time.wait(3000)
             return
-            
         
         pg.display.update()
         clock.tick(1000)
