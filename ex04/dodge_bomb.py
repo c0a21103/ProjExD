d845881bab664065e5ac629a6b707d77d0f2940a
koki <c0a2110338@edu.teu.ac.jp>
2022-12-13 16:40:43 +0900

    追加

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -14,7 +14,8 @@ def check_bound(obj_rct,scr_rct):
 
 def main():        
     clock = pg.time.Clock()
-    vx, vy = +1, +1
+    vx1, vy1 = +1, +1
+    vx2, vy2 = +1, +1
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
     scrn_rct = scrn_sfc.get_rect()
@@ -28,13 +29,18 @@ def main():
     
     bomb_sfc = pg.Surface((20,20))
     bomb_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bomb_sfc, (255,0,0),(10,10),size)
-    bomb_rct = bomb_sfc.get_rect()
-    bomb_rct.centerx = random.randint(0, scrn_rct.width)
-    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    pg.draw.circle(bomb_sfc, (255,0,0),(10,10),10)
+    pg.draw.circle(bomb_sfc, (255,0,0),(10,10),10)
+    bomb_rct_1 = bomb_sfc.get_rect()
+    bomb_rct_2 = bomb_sfc.get_rect()
+    bomb_rct_1.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct_1.centery = random.randint(0, scrn_rct.height)
+    bomb_rct_2.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct_2.centery = random.randint(0, scrn_rct.height)
     
-    
-    
+    fonto = pg.font.Font(None,100)
+    fonto.set_bold
+    txt = fonto.render("GAME OVER", True, (255,255,255))
     while True:
         scrn_sfc.blit(bg_sfc,bg_rct)
         for event in pg.event.get():
@@ -62,14 +68,22 @@ def main():
                 tori_rct.centerx -= 1
         scrn_sfc.blit(tori_sfc,tori_rct)
         
-        bomb_rct.move_ip(vx, vy)
-        scrn_sfc.blit(bomb_sfc,bomb_rct)
-        yoko, tate = check_bound(bomb_rct,scrn_rct)
-        vx *= yoko
-        vy *= tate
-        
-        if tori_rct.colliderect(bomb_rct):
+        bomb_rct_1.move_ip(vx1, vy1)
+        bomb_rct_2.move_ip(vx2, vy2)
+        scrn_sfc.blit(bomb_sfc,bomb_rct_1)
+        scrn_sfc.blit(bomb_sfc,bomb_rct_2)
+        yoko1, tate1 = check_bound(bomb_rct_1,scrn_rct)
+        yoko2, tate2 = check_bound(bomb_rct_2,scrn_rct)
+        vx1 *= yoko1
+        vy1 *= tate1
+        vx2 *= yoko2
+        vy2 *= tate2
+        if tori_rct.colliderect(bomb_rct_1) or tori_rct.colliderect(bomb_rct_2):
+            scrn_sfc.blit(txt,scrn_rct.center)
+            pg.display.update()
+            pg.time.wait(3000)
             return
+            
         
         pg.display.update()
         clock.tick(1000)
